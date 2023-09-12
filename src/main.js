095adb540279a19203dd21b72ed62123d730c778
deepwubs <pchucklambs@gmail.com>
2023-09-11 21:30:18 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser'
+
+import TitleScreen from './scenes/TitleScreen'
+import Game from './scenes/Game'
+
+const config = {
+    width: 800,
+    height: 500,
+    type: Phaser.AUTO,
+    backgroundColor: '#242323',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    }
+}
+
+const game = new Phaser.Game(config)
+
+game.scene.add('titlescreen', TitleScreen)
+game.scene.add('game', Game)
+
+// game.scene.start('titlescreen')
+game.scene.start('game')
